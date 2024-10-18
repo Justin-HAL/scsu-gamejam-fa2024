1adf6dc635e126f9e7fe99dee5279db86d2bb8a5
Justin Halvorson <justinhalvor@gmail.com>
2024-10-18 17:24:43 -0500

    Added health bar to player

--- player.gd
+++ player.gd
@@ -2,10 +2,11 @@ extends CharacterBody2D
 
 
 @export var speed = 200
-
+@export var health = 100
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 
+	$ProgressBar.value = health
 	move_and_slide()
