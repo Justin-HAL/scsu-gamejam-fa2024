3598c315090ce856ee84361275502e15b1a91c17
Justin Halvorson <justinhalvor@gmail.com>
2024-10-19 16:26:07 -0500

    Added resource nodes for eyeball, appear, and worms

--- Resource nodes/collection_resource.gd
+++ Resource nodes/collection_resource.gd
@@ -7,13 +7,16 @@ var can_collect = true
 var player_in_range = false
 var player = null
 
-	
+func _ready():
+	$Label.visible = false
+
 func _process(delta: float) -> void:
 	if player_in_range:
 		if Input.is_action_just_pressed("collect"):
 			if can_collect:
 				harvest()
 
+		
 func harvest():
 	can_collect = false
 	collected.emit()
@@ -27,12 +30,12 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_range = true
 		player = body
-
+		$Label.visible = true
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.has_method("player"):
 		player_in_range = false
 		player = null
-
+		$Label.visible = false
 
 func _on_regrow_timeout() -> void:
 	if !can_collect:
